1deb01bf228e24cd104f2abc7171e28ddbef1761
Nkunja~ <munene01denis@gmail.com>
2024-01-31 14:38:50 +0300

    Models Change

--- SyncHack/SyncHack/settings.py
+++ SyncHack/SyncHack/settings.py
@@ -84,6 +84,9 @@ DATABASES = {
 }
 
 
+# AUTH_USER_MODEL = 'work.CustomUser'
+
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
--- SyncHack/work/models.py
+++ SyncHack/work/models.py
@@ -1,4 +1,7 @@
+# from django.contrib.auth.models import AbstractUser, Group
 from django.db import models
+
+
 
 class Employee(models.Model):
     fname = models.CharField(max_length=10)
@@ -13,3 +16,17 @@ class Employee(models.Model):
     def __str__(self):
         return self.fname + ' ' + self.lname + ' ' + self.email
 
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('done', 'Done'),
+        ('incomplete', 'Incomplete'),
+    ]
+
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    assigned_to = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+
+    def __str__(self):
+        return self.name
