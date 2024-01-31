f2dc754392e25f762fd78a7b797b9fdb3fe7cc97
Hamid <45767512+AdeyinkaEsi1@users.noreply.github.com>
2024-01-31 12:44:24 +0100

    Update views.py

--- SyncHack/work/views.py
+++ SyncHack/work/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from .forms import SignUpForm
+from django.contrib import messages
+from .models import Employee
 
 
-def welcome_page(request):
-    return render(request, 'home.html')
+
+def Signupview(request):
+    all_employees = Employee.objects.all
+    if request.method == 'POST':
+        form = SignUpForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Registration Successful')
+            return render(request, 'work\dashboard.html', {'all': all_employees})
+    else:
+        form = SignUpForm()
+        return render(request, 'work\Signup.html')
+
