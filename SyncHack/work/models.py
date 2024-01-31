694cf48b914cb4101381628f038ae71de71caf51
Hamid <45767512+AdeyinkaEsi1@users.noreply.github.com>
2024-01-31 10:56:27 +0100

    Update models.py

--- SyncHack/work/models.py
+++ SyncHack/work/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    fname = models.CharField(max_length=10)
+    lname = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50, unique=True)
+    pword = models.CharField(max_length=50)
+
+    class Meta:
+        unique_together = ('fname', 'lname')
+
+
+    def __str__(self):
+        return self.fname + ' ' + self.lname + ' ' + self.email
+
